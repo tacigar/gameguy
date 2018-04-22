48ea24f2f6cdc612dd84d64147fb5ce4d716b4f3
tacigar <ig.lhound@gmail.com>
2018-04-22 16:44:41 +0900

    Add sample pixels

--- src/game.cpp
+++ src/game.cpp
@@ -16,6 +16,13 @@ auto Game::initialize() -> void
         SDL_WINDOW_SHOWN
     );
     m_graphics = new Graphics(m_window, 640, 480);
+
+    for (int i = 0; i < m_graphics->getTextureWidth(); ++i) {
+        for (int j = 0; j < m_graphics->getTextureHeight(); ++j) {
+            if (i == j)
+                m_graphics->setPixel(i, j, 0);
+        }
+    }
 }
 
 auto Game::run() -> void
