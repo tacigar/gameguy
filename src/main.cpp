400591196ff57ba4b647572b765b197c8f881af4
tacigar <ig.lhound@gmail.com>
2018-04-22 10:43:11 +0900

    Fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,16 @@
 
 auto main(int argc, char **argv) -> int
 {
-  SDL_Window* window = NULL;
-  SDL_Surface* screenSurface = NULL;
-  SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("The exciting white window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-  screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF));
-  SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return 0;
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("The exciting white window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 
 }
