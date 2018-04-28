a177cfb1573779300140d4eb0fb5667874a7672e
tacigar <ig.lhound@gmail.com>
2018-04-28 10:46:02 +0900

    Add register class

---
+++ src/cpu/register.hpp
@@ -0,0 +1,17 @@
+#ifndef GAMEGUY_CPU_REGISTER_HPP
+#define GAMEGUY_CPU_REGISTER_HPP
+
+namespace gameguy {
+
+namespace cpu {
+
+class Register
+{
+
+};
+
+} // namespace cpu
+
+} // namespace gameguy
+
+#endif // GAMEGUY_CPU_REGISTER_HPP
