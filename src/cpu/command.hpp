ae4d382d324cc50d08353e47ecdba0d74e20cc8a
tacigar <ig.lhound@gmail.com>
2018-04-29 08:55:35 +0900

    Add command class

---
+++ src/cpu/command.hpp
@@ -0,0 +1,19 @@
+#ifndef GAMEGUY_CPU_COMMAND_HPP
+#define GAMEGUY_CPU_COMMAND_HPP
+
+namespace gameguy {
+
+namespace cpu {
+
+class Command
+{
+  public:
+
+  private:
+};
+
+} // namespace cpu
+
+} // namespace gameguy
+
+#endif // GAMEGUY_CPU_COMMAND_HPP
