0f310762c9294259d6c501c81dadf7ff56493433
tacigar <ig.lhound@gmail.com>
2018-04-28 18:18:00 +0900

    Add rom header class

---
+++ src/rom/header.hpp
@@ -0,0 +1,17 @@
+#ifndef GAMEGUY_ROM_HEADER_HPP
+#define GAMEGUY_ROM_HEADER_HPP
+
+namespace gameguy {
+
+namespace rom {
+
+class Header
+{
+    
+};
+
+} // namespace rom
+
+} // namespace gameguy
+
+#endif // GAMEGUY_ROM_HEADER_HPP
