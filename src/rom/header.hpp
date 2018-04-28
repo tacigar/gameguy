7d4928487b572f5b2d9a5d9801d9ba9d22abd0b6
tacigar <ig.lhound@gmail.com>
2018-04-28 18:46:31 +0900

    Add rom header member

--- src/rom/header.hpp
+++ src/rom/header.hpp
@@ -1,13 +1,22 @@
 #ifndef GAMEGUY_ROM_HEADER_HPP
 #define GAMEGUY_ROM_HEADER_HPP
 
+#include <cstddef>
+#include <string>
+
 namespace gameguy {
 
 namespace rom {
 
 class Header
 {
-    
+  private:
+    std::size_t m_romSize;
+    std::size_t m_ramSize;
+    std::string m_title;
+    std::string m_manufacturerCode;
+    bool m_cgbFlag;
+    bool m_sgbFlag;
 };
 
 } // namespace rom
