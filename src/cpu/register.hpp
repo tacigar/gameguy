77bbe107c1e9dda2acb10b51b1bed622409a8ff5
tacigar <ig.lhound@gmail.com>
2018-04-28 10:55:24 +0900

    Add register field

--- src/cpu/register.hpp
+++ src/cpu/register.hpp
@@ -1,13 +1,26 @@
 #ifndef GAMEGUY_CPU_REGISTER_HPP
 #define GAMEGUY_CPU_REGISTER_HPP
 
+#include <cstdint>
+
 namespace gameguy {
 
 namespace cpu {
 
 class Register
 {
+  private:
+    char m_regA;
+    char m_regB;
+    char m_regC;
+    char m_regD;
+    char m_regE;
+    char m_regF;
+    char m_regH;
+    char m_regL;
 
+    std::int_least16_t m_regPC;
+    std::int_least16_t m_regSP;
 };
 
 } // namespace cpu
