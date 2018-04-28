462c105aafdce999f1325ba18ad28d91a825f12c
tacigar <ig.lhound@gmail.com>
2018-04-28 19:31:20 +0900

    Add getter methods to ron.header class

--- src/rom/header.hpp
+++ src/rom/header.hpp
@@ -11,6 +11,47 @@ namespace rom {
 
 class Header
 {
+  public:
+    Header() = default;
+    ~Header() = default;
+
+    static auto parseData(std::vector<gameguy::Byte> rawData) -> Header;
+
+    auto romSize() const -> std::size_t
+    {
+        return m_romSize;
+    }
+
+    auto ramSize() const -> std::size_t
+    {
+        return m_ramSize;
+    }
+
+    auto title() const -> std::string
+    {
+        return m_title;
+    }
+
+    auto manufacturerCode() const -> std::string
+    {
+        return m_manufacturerCode;
+    }
+
+    auto cbgFlag() const -> bool
+    {
+        return m_cgbFlag;
+    }
+
+    auto sgbFlag() const -> bool
+    {
+        return m_sgbFlag;
+    }
+
+    auto cartridgeType() const -> gameguy::rom::CartridgeType
+    {
+        return m_cartridgeType;
+    }
+    
   private:
     std::size_t m_romSize;
     std::size_t m_ramSize;
