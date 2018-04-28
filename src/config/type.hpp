530d59ffe8ee2130396ccedbaa95d8d36db4f498
tacigar <ig.lhound@gmail.com>
2018-04-28 11:07:27 +0900

    Add 1-byte type and 2-byte type

---
+++ src/config/type.hpp
@@ -0,0 +1,11 @@
+#ifndef GAMEGUY_CONFIG_TYPE_HPP
+#define GAMEGUY_CONFIG_TYPE_HPP
+
+namespace gameguy {
+
+using Byte = uint8_t;
+using Word = uint16_t;
+
+} // namespace gameguy
+
+#endif // GAMEGUY_CONFIG_TYPE_HPP
