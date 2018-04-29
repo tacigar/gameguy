f2dbaea244daf1b91e8efb65b99f44fc22da0877
tacigar <ig.lhound@gmail.com>
2018-04-29 09:31:37 +0900

    Add command class constructor

---
+++ src/cpu/command.cpp
@@ -0,0 +1,15 @@
+#include "command.hpp"
+
+namespace gameguy {
+
+namespace cpu {
+
+Command::Command(gamegoy::cpu::Register register)
+        : m_register(register)
+{
+
+}
+
+} // namespace cpu
+
+} // namespace gameguy
--- src/cpu/command.hpp
+++ src/cpu/command.hpp
@@ -1,6 +1,12 @@
 #ifndef GAMEGUY_CPU_COMMAND_HPP
 #define GAMEGUY_CPU_COMMAND_HPP
 
+#include "../config/type.hpp"
+#include "register.hpp"
+
+#include <functional>
+#include <unordered_map>
+
 namespace gameguy {
 
 namespace cpu {
@@ -8,8 +14,14 @@ namespace cpu {
 class Command
 {
   public:
+    Command(gamegoy::cpu::Register register);
 
   private:
+    auto setupLoadCommands() -> void;
+
+  private:
+    gameguy::cpu::register m_register;
+    std::unordered_map<gameguy::Byte, std::function<void()>> m_commands;
 };
 
 } // namespace cpu
