5f9dcc88f2b7048daf10dd709198be9ef991d3d7
tacigar <ig.lhound@gmail.com>
2018-05-10 00:49:08 +0900

    Add rom class

---
+++ src/rom/rom.hpp
@@ -0,0 +1,17 @@
+#ifndef GAMEGUY_ROM_ROM_HPP
+#define GAMEGUY_ROM_ROM_HPP
+
+namespace gameguy {
+
+namespace rom {
+
+class Rom
+{
+
+};
+
+} // namespace rom
+
+} // namespace gameguy
+
+#endif // GAMEGUY_ROM_ROM_HPP
