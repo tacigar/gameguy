dca8d5f1a565308cc3f52036fe7729295b9a0f20
tacigar <ig.lhound@gmail.com>
2018-05-10 01:31:45 +0900

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,18 @@
 #include "game.hpp"
 #include "config/type.hpp"
 #include "rom/header.hpp"
+#include "rom/rom.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
 
 auto main(int argc, char **argv) -> int
 {
-    std::ifstream fin("rom/adjustris.gb", std::ios::in | std::ios::binary);
-    if (!fin) {
-        std::cout << "cannot open file" << std::endl;
-        return 1;
-    }
-    std::vector<gameguy::Byte> data;    
-    while (!fin.eof()) {
-        char buf;
-        fin.read((char *) &buf, sizeof(char));
-        data.push_back(buf);
-    }
-    gameguy::rom::Header header = gameguy::rom::Header::parseData(data);
-    std::cout << header.romSize() << std::endl;
-    std::cout << header.ramSize() << std::endl;
-    std::cout << header.title() << std::endl;
-    std::cout << header.manufacturerCode() << std::endl;
+    std::shared_ptr<gameguy::rom::Rom> rom = gameguy::rom::Rom::loadFile("rom/adjustris.gb");
+    std::shared_ptr<gameguy::rom::Header> header = rom->header();
 
+    std::cout << *header << std::endl;
+    
     gameguy::Game game;
     game.initialize();
     game.run();
