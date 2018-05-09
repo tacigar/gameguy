0d2fd15c091b1c818797a78e67492db6f9d8711f
tacigar <ig.lhound@gmail.com>
2018-05-10 00:12:25 +0900

    Add cpu class

---
+++ src/cpu/cpu.hpp
@@ -0,0 +1,17 @@
+#ifndef GAMEGUY_CPU_CPU_HPP
+#define GAMEGUY_CPU_CPU_HPP
+
+namespace gameguy {
+
+namespace cpu {
+
+class Cpu
+{
+
+};
+
+} // namespace cpu
+
+} // namespace gameguy
+
+#endif // GAMEGUY_CPU_CPU_HPP
