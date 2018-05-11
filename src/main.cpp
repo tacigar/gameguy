61a7981007a8d64d14f6e7900dfab4ed9eb7583a
tacigar <ig.lhound@gmail.com>
2018-05-11 14:10:02 +0900

    Update main to use debug macro

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include "game.hpp"
 #include "config/type.hpp"
+#include "config/config.hpp"
 #include "rom/header.hpp"
 #include "rom/rom.hpp"
 #include <iostream>
@@ -12,7 +13,7 @@ auto main(int argc, char **argv) -> int
     std::shared_ptr<gameguy::rom::Rom> rom = gameguy::rom::Rom::loadFile("rom/adjustris.gb");
     std::shared_ptr<gameguy::rom::Header> header = rom->header();
 
-    std::cout << *header << std::endl;
+    DEBUG_STDOUT(*header << std::endl);
     
     gameguy::Game game;
     game.initialize();
