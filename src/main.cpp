30f8a0d9ca39170bf5f415a6826660f006f5c634
tacigar <ig.lhound@gmail.com>
2018-05-12 00:13:36 +0900

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
-#include <SDL2/SDL.h>
 #include "game.hpp"
 #include "config/config.hpp"
+#include "cpu/cpu.hpp"
 #include "rom/header.hpp"
 #include "rom/rom.hpp"
+
+#include <SDL2/SDL.h>
 #include <iostream>
 #include <fstream>
+#include <memory>
 #include <vector>
 
 auto main(int argc, char **argv) -> int
@@ -13,6 +16,11 @@ auto main(int argc, char **argv) -> int
     std::shared_ptr<gameguy::rom::Header> header = rom->header();
 
     DEBUG_STDOUT(*header << std::endl);
+
+    gameguy::cpu::Cpu cpu(rom->rawData());
+    cpu.executeInstruction();
+    cpu.executeInstruction();
+    cpu.executeInstruction();
     
     gameguy::Game game;
     game.initialize();
